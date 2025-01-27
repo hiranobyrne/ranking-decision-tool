2e460d59580ffd13aa0b8bc4a7dad210b2aabaf2
hiranobyrne <hiranobyrne@gmail.com>
2025-01-27 15:16:27 +0000

    Update utils.py

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -7,57 +7,12 @@ import csv
 import zipfile
 import joblib
 
-# Function to extract models if not already extracted
-def extract_model(zip_path, extract_to):
-    if not os.path.exists(extract_to):
-        with zipfile.ZipFile(zip_path, "r") as zip_ref:
-            zip_ref.extractall(os.path.dirname(extract_to))
-
-# Function to load model
-def load_model(model_path):
-    return joblib.load(model_path)
-
 
 def get_timestamp():
     # Funcao que retorna time stamp atual.
     now = datetime.now()
     return now.strftime("%d/%m/%Y, %H:%M:%S")
-
-
-# Function to fetch car images
-def fetch_car_image(brand, trade_name):
-    # Replace these with your actual API key and CSE ID
-    api_key = "AIzaSyDnHUNpYQyvFzmDBswYR4ygLZ_kskkcHz8"
-    cse_id = "90f8be4f8e9874169"
-
-    search_query = f"{brand} {trade_name} car"
-    url = "https://www.googleapis.com/customsearch/v1"
-
-    params = {
-        "key": api_key,
-        "cx": cse_id,
-        "q": search_query,
-        "searchType": "image",  # Fetch only images
-        "num": 1  # Limit to the first result
-    }
-
-    try:
-        response = requests.get(url, params=params)
-        # print(f"API Request URL: {response.url}")  # Debugging
-        # print(f"API Response Status Code: {response.status_code}")
-
-        if response.status_code == 200:
-            data = response.json()
-            # print("Response:", data)
-            if "items" in data and len(data["items"]) > 0:
-                # Extract the first image URL
-                return data["items"][0]["link"]
-        print("No image found. Using placeholder.")
-        return "https://via.placeholder.com/150"  # Default placeholder image
-    except Exception as e:
-        print(f"Error fetching image: {e}")
-        return "https://via.placeholder.com/150"
-
+    
 
 def load_dataset(data_path):
     """
